6cccb070a4bc9ede3906f8ee7e8ef652fe788823
Vahid <vahid.vaezian@gmail.com>
2018-05-24 11:58:28 -0700

    Update test.php

--- test.php
+++ test.php
@@ -1,17 +1,27 @@
-<?php
-    if ($handle = opendir('.')) {
-    while (false !== ($file = readdir($handle)))
-    {
-        if ($file != "." && $file != "..")
-        {
-            $thelist .= '<LI><a href="'.$file.'">'.$file.'</a>';
-        }
-    }
-    closedir($handle);
-    }
-?>
+<!DOCTYPE html>
+<html>
 
-<P>Dir:</p>
-<UL>
-<P><?=$thelist?></p>
-</UL>
+<head>
+  <title>test</title>
+</head>
+
+    <body>
+        <?php
+            if ($handle = opendir('.')) {
+            while (false !== ($file = readdir($handle)))
+            {
+                if ($file != "." && $file != "..")
+                {
+                    $thelist .= '<LI><a href="'.$file.'">'.$file.'</a>';
+                }
+            }
+            closedir($handle);
+            }
+        ?>
+
+        <P>Dir:</p>
+        <UL>
+        <P><?=$thelist?></p>
+        </UL>
+    </body>
+</html>
