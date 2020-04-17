a62fb004552b57ab45f2585674ad4a430a6ffee5
Vahid <vahid.vaezian@gmail.com>
2020-04-16 19:15:29 -0700

    Update Linked List.py

--- Data-Structures-Implementations/Linked List.py
+++ Data-Structures-Implementations/Linked List.py
@@ -8,11 +8,13 @@ class LinkedList:
   def __init__(self):
     self.head = None
 
+    
   def insertAtFront(self, val):
     n = Node(val)
     n.nxt = self.head
     self.head = n
 
+    
   def append_to_end(self, val):
     n = Node(val)
     if not self.head:
@@ -23,6 +25,7 @@ class LinkedList:
         cur = cur.nxt
       cur.nxt = n
 
+      
   def insert_after(self, val1, val2):
     """ inserts a node with value val_1 after the node with value val_2
     Assuming all values are distinct. If this is not the case, inserts after the first occurence.
@@ -39,28 +42,30 @@ class LinkedList:
     print("Didn't find a node with value {}".format(val1))
     return False
 
-    def insertBehind(self, val_1, val_2):
-        """ Assuming all values are distinct. If not, we need to use "keys" for nodes.
-            inserts a node with value val_1 behind the node with value val_2"""
-        if self.head is None:
-            print "There is no node with value " + str(val_2)
-        else:
-            if val_2 == self.head:
-                self.insertAtFront(val_1)
-            else:
-                cur = self.head
-                while True:
-                    if cur.nxt.val == val_2:
-                        tmp = Node(val_1)
-                        tmp.nxt = cur.nxt
-                        cur.nxt = tmp
-                        break
-                    else:
-                        if cur.nxt:
-                            cur = cur.nxt
-                        else:
-                            print "There is no node with value " + str(val_2)
-                            break
+  
+  def insert_behind(self, val1, val2):
+    """ inserts a node with value val_1 after the node with value val_2
+    Same remarks as for the insert_after function
+    """
+    if not self.head:
+      print('The linkedlist is empty.')
+      return False
+    cur = self.head
+    if cur.val == val1:
+      n = Node(val2)
+      self.head = n
+      n.nxt = cur
+      return True
+    while cur.nxt:
+      if cur.nxt.val == val1:
+        n = Node(val2)
+        n.nxt = cur.nxt
+        cur.nxt = n
+        return True
+      cur = cur.nxt
+    print("Didn't find a node with value {}".format(val1))
+    return False
+
 
   def reverse(self, in_place=True):
     import copy
@@ -99,6 +104,7 @@ class LinkedList:
 
       return self
 
+    
   def delete_node(self, val1):
     if self.head.val == val1:
       self.head = self.head.nxt
@@ -113,6 +119,7 @@ class LinkedList:
     print("Didn't find a node with val {}".format(val1))
     return False
 
+  
   def __str__(self):
     if self.head is None:
       return "The linked list is empty."
@@ -134,7 +141,11 @@ ll.insert_after(0, 5)
 ll.insert_behind(1, 9)
 ll.insert_behind(0, -1)
 print(ll)
-ll.reverse()
+# ll.reverse()
+# print(ll)
+# print(ll.reverse(in_place=False))
+ll.delete_node(9)
 print(ll)
-print(ll.reverse(in_place=False))
+ll.delete_node(-1)
+print(ll)
 
