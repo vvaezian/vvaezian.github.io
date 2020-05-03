c4e2130d0c564578df4967a6f628ebe5c612e115
Vahid <vahid.vaezian@gmail.com>
2020-05-03 16:12:50 -0700

    Update 4.2.py

--- Cracking-the-Coding-Interview/4.2.py
+++ Cracking-the-Coding-Interview/4.2.py
@@ -1,19 +1,22 @@
 # Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an
 # algorithm to create a binary search tree with minimal height.
 
-def minHeightBST(sortedList):
+class tNode:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
 
-  def _minHeightBST(sortedList, bst):
-    if sortedList:
-        mid = len(sortedList) // 2
-        bst.add(sortedList[mid])
-        if mid > 0:
-          _minHeightBST(sortedList[:mid], bst)
-          _minHeightBST(sortedList[mid + 1:], bst)
-          
-  bst = BST()
-  _minHeightBST(sortedList, bst)
-  return bst
 
-# we could use only start and end variables instead of passing half the list, but since the function is returning O(n) data,
-# it doesn't make a difference in space complexity.
+def minimal_tree(a_list):
+
+  if len(a_list) == 1:
+    return tNode(a_list[0])
+  if len(a_list) == 0:
+    return None
+
+  m_idx = len(a_list)//2
+  m_node = tNode(a_list[m_idx])
+  m_node.left, m_node.right = minimal_tree(a_list[:m_idx]), minimal_tree(a_list[m_idx + 1:])
+
+  return m_node
