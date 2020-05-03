412a0b6cc0363de3f89e1734b35f39394285633b
Vahid <vahid.vaezian@gmail.com>
2020-05-03 16:13:14 -0700

    Update 4.2.py

--- Cracking-the-Coding-Interview/4.2.py
+++ Cracking-the-Coding-Interview/4.2.py
@@ -8,7 +8,7 @@ class tNode:
     self.right = None
 
 
-def minimal_tree(a_list):
+def minBST(a_list):
 
   if len(a_list) == 1:
     return tNode(a_list[0])
@@ -17,6 +17,6 @@ def minimal_tree(a_list):
 
   m_idx = len(a_list)//2
   m_node = tNode(a_list[m_idx])
-  m_node.left, m_node.right = minimal_tree(a_list[:m_idx]), minimal_tree(a_list[m_idx + 1:])
+  m_node.left, m_node.right = minBST(a_list[:m_idx]), minBST(a_list[m_idx + 1:])
 
   return m_node
