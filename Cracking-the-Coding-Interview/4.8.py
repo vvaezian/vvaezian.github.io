3582466cac0b9e1e3dc3da5e5980084d6e64a30c
Vahid <vahid.vaezian@gmail.com>
2020-05-04 19:10:52 -0700

    Update 4.8.py

--- Cracking-the-Coding-Interview/4.8.py
+++ Cracking-the-Coding-Interview/4.8.py
@@ -2,12 +2,16 @@
 # of two nodes in a binary tree. Avoid storing additional nodes in a data structure. 
 # NOTE: This is not necessarily a binary search tree.
 
+# If nodes have parents, it's easy. In the following we assume nodes don't have parents.
+# If it hadn't said "Avoid storing additional nodes in a data structure." we could simulate the tree with another identical tree
+# but where nodes have parents (building a mapping between original nodes and simulated nodes along the way) 
+# and then solve the problem similar to the case when nodes have parents.
+# But since we clearly have been instructed not to do that we can use the follwoing approach (assumes we have access to the 
+# root of the tree):
+# We first find the paths to the nodes from the root. Then we compare the path and find the the first common ancesstor
+# based on that
 
-# I'm not assuming that the nodes are given, but the NodeKeys are given (this makes more sense to me). 
-# So We first find the nodes. While doing this we record the path to them as well. Then we compare the path and find
-# the the first common ancesstor based on that
-
-def firstCommonAncestor(BinaryTreeInstance, key1, key2):
+def firstCommonAncestor(BinaryTreeInstance, n1, n2):
 
   def _findPathToNodeKey(key, rootNode, path=[]):
     if rootNode:
