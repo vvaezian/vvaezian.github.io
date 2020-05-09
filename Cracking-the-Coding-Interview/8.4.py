8550a8a21d7af4213d3cbb7416041fb3bbba484f
Vahid <vahid.vaezian@gmail.com>
2020-05-09 14:32:04 -0700

    Update 8.4.py

--- Cracking-the-Coding-Interview/8.4.py
+++ Cracking-the-Coding-Interview/8.4.py
@@ -1,5 +1,20 @@
 # Power Set: Write a method to return all subsets of a set.
 
+
+### Recursive
+def subset(array):
+
+  def _subset(array, out):
+    if len(array) == 0:
+      return [[]]
+    for item in [ i + [array[-1]] for i in _subset(array[:-1], out) ]:
+      out.append(item)
+    return out
+  
+  return _subset(array, [[]])
+
+
+### Iterative
 def subsets(list1):
     import copy
     output = [[]]
