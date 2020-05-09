f7f6a0807f0bbcec33817e25f8cb262747c8318e
Vahid <vahid.vaezian@gmail.com>
2020-05-09 12:44:02 -0700

    Create Binary_Search.py

---
+++ Code Examples/Binary_Search.py
@@ -0,0 +1,23 @@
+def bi_search(x, array):
+
+  def _bi_search(lo, hi):
+    if lo == hi:
+      if array[lo] == x:
+        return lo
+    if lo > hi:
+      return False
+            
+    mid = (lo + hi) // 2
+    if  array[mid] == x:
+      return mid
+    elif x < array[mid]:
+      return _bi_search(lo, mid - 1)
+    else:
+      return _bi_search(lo + 1, hi)
+
+  if len(array) == 0:
+    return False
+  return _bi_search(0, len(array) - 1)
+
+
+print(bi_search(10, [10, 20]))
