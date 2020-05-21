a39246cc27705b30b93d56b0999ab938b48456da
Vahid <vahid.vaezian@gmail.com>
2020-05-21 16:27:15 -0700

    Update 9-Max Sum of Disjoint Numbers.py

--- Code Examples/Daily Coding Problem/9-Max Sum of Disjoint Numbers.py
+++ Code Examples/Daily Coding Problem/9-Max Sum of Disjoint Numbers.py
@@ -1,6 +1,10 @@
 # Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
 # E.g. [2, 4, 6, 2, 5] should return 13.
 
+# Analysis: Let opt(n) be the solution for an array of n numbers. Either the nth element is part of the solution or not. 
+# If it isn't, then opt(n) = opt(n-1)
+# If it is, then the (n-1)th number cannot be chosen. We have two cases: opt(n) = n + opt(n - 2) or opt(n) = n + opt(n-3)
+# E.g. if we have [1, 10, 2, 5, 10] the second case happens
 
 # O(3^n) time, O(1) space
 def max_disjoint_sum(array):
@@ -19,6 +23,19 @@ def max_disjoint_sum(array):
 
   return helper(array, len(array) - 1)
 
-# O(n) time, O(n) space
 
-# O(n) time, O(1) space
+
+# O(n) time, O(n) space (memoization) 
+# recursive version can use memoization as well, but the iterative approach is simpler
+def max_disjoint_sum(arr):
+  m = [0] * len(arr)
+  m[0] = arr[0]
+  m[1] = max(arr[0], arr[1])
+  m[2] = max(arr[0] + arr[2], arr[1])
+  for i in arr[3:]:
+    m[i] = max(m[i-1], max(arr[i] + m[i-2], arr[i] + m[i-3]))
+  return m[-1]
+
+# O(n) time, O(1) space (memoization and space-efficient)
+# since we are only looking at last three elements of m, we don't need to use the whole  m
+
