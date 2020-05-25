2dbed3838a18b07715c749b4dc5ecb9955650d15
Vahid <vahid.vaezian@gmail.com>
2020-05-24 19:48:17 -0700

    Create 12-StairCase.py

---
+++ Code Examples/Daily Coding Problem/12-StairCase.py
@@ -0,0 +1,16 @@
+# There exists a staircase with N steps, and you could climb any number from a set of positive integers X
+# Given N and X, write a function that returns the number of unique ways you can climb the staircase.
+
+
+def stairs_with_steps(n, steps):
+  # Since we want to look back at most max(steps) steps, 
+  # we add this many zeros more so we don't run out of index.
+  m = [0] * (max(steps) + n)  # the cache
+
+  for i in range(max(steps), n + max(steps)):
+    for s in steps:
+      m[i] += m[i - s]
+    if i - max(steps) + 1 in steps:
+      m[i] += 1
+  
+  return m[-1]
