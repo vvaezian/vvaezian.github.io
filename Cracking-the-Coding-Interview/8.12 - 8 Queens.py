4d3f07ce47a3ff3cc6d6718a571f4831f4983956
Vahid <vahid.vaezian@gmail.com>
2020-06-01 19:44:17 -0700

    Update and rename 8.12 - Eight Queens.py to 8.12 - 8 Queens.py

--- Cracking-the-Coding-Interview/8.12 - 8 Queens.py
+++ Cracking-the-Coding-Interview/8.12 - 8 Queens.py
@@ -38,3 +38,25 @@ def eightQ():
   board = [ [0] * 8 for _ in range(8) ]
   _eightQ(board, 0)
   return total[0], answers
+
+# Simpler Approach (Credit: Daily Coding Problem)
+def is_valid(board):
+    current_queen_row, current_queen_col = len(board) - 1, board[-1]
+    # Check if any queens can attack the last queen.
+    for row, col in enumerate(board[:-1]):
+        diff = abs(current_queen_col - col)
+        if diff == 0 or diff == current_queen_row - row:
+            return False
+    return True
+  
+def n_queens(n, board=[]):
+    if n == len(board):
+        return 1
+
+    count = 0
+    for col in range(n):
+        board.append(col)
+        if is_valid(board):
+            count += n_queens(n, board)
+        board.pop()
+    return count
