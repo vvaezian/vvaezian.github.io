601a744786d87b684a8ebff9d532e7d018ec3d48
Vahid <vahid.vaezian@gmail.com>
2020-06-02 19:49:49 -0700

    Update 07-decode.py

--- Code Examples/Daily Coding Problem/07-decode.py
+++ Code Examples/Daily Coding Problem/07-decode.py
@@ -1,5 +1,7 @@
-# Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
-# For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+'''
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+'''
 
 # recursive O(n) runtime, O(n) space
 def decode(string:str) -> int:
