2747e1123ebd02f3478eff02b90aa5e2c446b900
Vahid <vahid.vaezian@gmail.com>
2020-06-02 19:50:26 -0700

    Update 12-StairCase.py

--- Code Examples/Daily Coding Problem/12-StairCase.py
+++ Code Examples/Daily Coding Problem/12-StairCase.py
@@ -1,6 +1,7 @@
-# There exists a staircase with N steps, and you could climb any number from a set of positive integers X
-# Given N and X, write a function that returns the number of unique ways you can climb the staircase.
-
+'''
+There exists a staircase with N steps, and you could climb any number from a set of positive integers X
+Given N and X, write a function that returns the number of unique ways you can climb the staircase.
+'''
 
 def stairs_with_steps(n, steps):
   # Since we want to look back at most max(steps) steps, 
