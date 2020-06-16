e74294199721f028b27c6d452620d0c66becbd4d
Vahid <vahid.vaezian@gmail.com>
2020-06-15 21:52:05 -0700

    Update 34-Palindrome_Min.py

--- Code Examples/Daily Coding Problem/34-Palindrome_Min.py
+++ Code Examples/Daily Coding Problem/34-Palindrome_Min.py
@@ -15,13 +15,21 @@ def palindrome_min(string):
   return helper(string, {})
 
 def helper(string, cache):
+  # cache hit
   if string in cache:
     return cache[string]
+  
+  # base case
   if len(string) <= 1:
     return string
+  
+  # if first and last are the same we don't need to insert anything
   if string[0] == string[-1]:
     return string[0] + helper(string[1:-1], cache) + string[-1]
   
+  # for the string [1..n] find which one of [2..n] or [1..n-1] is the shorter palindrome
+  # and return it together with the appropriate letter padded at both sides
+  # if they are the same length return the one that its padding is lexicography earlier
   n1 = helper(string[1:], cache)
   cache[string[1:]] = n1
   n2 = helper(string[:-1], cache)
@@ -30,6 +38,7 @@ def helper(string, cache):
     return string[0] + n1 + string[0]
   if len(n2) < len(n1):
     return string[-1] + n2 + string[-1]
+  # This part if for returning the lexicographically earliest one
   if len(n1) == len(n2):
     return string[0] + n1 + string[0] if string[0] < string[-1] else string[-1] + n2 + string[-1]
 
