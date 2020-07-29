7a9bad9a11ff62c94d9a193f04657bf9d68bd346
Vahid <vahid.vaezian@gmail.com>
2020-07-29 11:45:37 -0700

    Update 03-SERDE.py

--- Code Examples/Daily Coding Problem/03-SERDE.py
+++ Code Examples/Daily Coding Problem/03-SERDE.py
@@ -1,5 +1,6 @@
 '''
-Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
+and deserialize(s), which deserializes the string back into the tree.
 For example, given the following Node class
 class Node:
     def __init__(self, val, left=None, right=None):
